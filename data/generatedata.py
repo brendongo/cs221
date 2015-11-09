f4c3010933fd5fb8c1b5c4d21cdf87acf29ee33b
Brendon Go <brendon.go@gmail.com>
2015-11-09 03:03:58 -0800

    Finished Data Generation Code

--- data/generatedata.py
+++ data/generatedata.py
@@ -1,29 +1,69 @@
-import re,  mmap
+import re,  mmap, random, string
 
+NOISE = 0.05
 
-class InputFile():
-    def __init__(self, filename):
-        self.texts = []
-        self.index = 0
-        with open(filename, 'r+') as f:
-            data = mmap.mmap(f.fileno(),0)
-            bodyRegex = re.compile("<BODY>([^<]*)</BODY>", re.IGNORECASE)
-            matches = bodyRegex.findall(data)
-            for match in matches:
-                text = "%s" % match
-                self.texts.append(text)
+def get_texts(filename):
+    texts = []
+    with open(filename, 'r+') as f:
+        data = mmap.mmap(f.fileno(),0)
+        bodyRegex = re.compile("<BODY>([^<]*)</BODY>", re.IGNORECASE)
+        matches = bodyRegex.findall(data)
+        for match in matches:
+            text = "%s" % match
+            texts.append(text)
+    return texts
 
-    def next_string(self):
-        if self.index < len(self.texts):
-            index = self.index
-            self.index += 1
-            return self.texts[index]
-        
+def generateKey():
+    key = list(string.ascii_uppercase)
+    random.shuffle(key)
+    return ''.join(key)
+
+# Subsitutes character if mapping exists from current to substitution
+# Assumes current and substitution are Capital Strings with no repeating characters
+# Case of return character is same as input character
+def substitute(character, current, substitution):
+    if character.upper() not in current:
+        return character
+    index_in_substitution = current.find(character.upper())
+    if character.isupper():
+        return substitution[index_in_substitution]
+    return substitution[index_in_substitution].lower()
+
+def encrypt(text, key):
+    result = ""
+    for char in text:
+        letter = substitute(char, string.ascii_uppercase, key)
+        result += letter
+    return result
+
+def add_noise(text, noise):
+    result = ""
+    for letter in text:
+        if letter in string.ascii_letters and random.random() < noise:
+            random_letter = randomLetter()
+            letter = random_letter.upper() if letter.isupper() else random_letter.lower()
+        result += letter
+    return result
+
+def randomLetter():
+    return random.choice(string.ascii_letters)
 
 def main():
-    inputs_file = InputFile("reut2-000.sgm")
-    print inputs_file.next_string()
+    output = open("output.csv", 'w')
+    output_noise = open("output_noise.csv", 'w')
 
+    for i in range(22):
+        filename = "reut2-0%02d.sgm" % i 
+        texts = get_texts(filename)
+        for text in texts:
+            text = ' '.join(text.split())
+            key = generateKey()
+            cipher_text = encrypt(text, key)
+            noised = add_noise(cipher_text, NOISE)
+            line =  "%s, %s, %s\n" % (text, key, cipher_text)
+            line_noised = "%s, %s, %s\n" % (text, key, noised)
+            output.write(line)
+            output_noise.write(line_noised)
 
 
 
