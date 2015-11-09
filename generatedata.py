13f4a3f6e8f99ffd721dee2b8efaedfb62f1b736
Brendon Go <brendon.go@gmail.com>
2015-11-08 23:39:02 -0800

    Starting Data Generation Code

---
+++ generatedata.py
@@ -0,0 +1,31 @@
+import re,  mmap
+
+
+class InputFile():
+    def __init__(self, filename):
+        self.texts = []
+        self.index = 0
+        with open(filename, 'r+') as f:
+            data = mmap.mmap(f.fileno(),0)
+            bodyRegex = re.compile("<BODY>([^<]*)</BODY>", re.IGNORECASE)
+            matches = bodyRegex.findall(data)
+            for match in matches:
+                text = "%s" % match
+                self.texts.append[text]
+
+    def next_string(self):
+        if self.index < len(self.text):
+            index = self.index
+            self.index += 1
+            return self.text[index]
+        
+
+def main():
+    inputs_file = InputFile()
+    print inputs_file.next_string()
+
+
+
+
+if __name__ == '__main__':
+    main()
