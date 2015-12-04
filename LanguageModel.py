3180b4da19e50c8092d4877b487bf793d9c2a777
Colin Man <colinman18@gmail.com>
2015-12-03 20:28:14 -0800

    gibbs works

--- LanguageModel.py
+++ LanguageModel.py
@@ -37,122 +37,85 @@ class LanguageModel:
 
         trigram = first + "&" + second + "&" + third
         self.trigramCounts[trigram] += 1
-        
+
         bigram = first + "&" + second
         self.bigramCounts[bigram] += 1
-        
+
         self.unigramCounts[first] += 1
         self.unigramtotal += 1
 
-  
+
     # Code below to initialize with ngrams file with format count word word word in each line
     # nGramsFile = open(corpus, 'r')
     # for line in nGramsFile:
     #   count, first, second, third = line.split()
     #   count = int(count)
-      
+
     #   trigram = first + "&" + second + "&" + third
     #   self.trigramCounts[trigram] += count
-      
+
     #   bigram = first + "&" + second
     #   self.bigramCounts[bigram] += count
     #   bigram = second + "&" + third
     #   self.bigramCounts[bigram] += count
-      
+
     #   self.unigramCounts[first] += count
     #   self.unigramCounts[second] += count
     #   self.unigramCounts[third] += count
-    #   self.unigramtotal += 3*count     
+    #   self.unigramtotal += 3*count
 
   def score(self, sentence):
     # sentence is a string
-    
+
     characterScore = 1.0
     # Character Score
     for i in xrange(0, len(sentence) - 2):
       first = sentence[i]
       second = sentence[i + 1]
       third = sentence[i + 2]
-      
+
       trigram = first + "&" + second + "&" + third
       trigramcount = self.characterTrigramCounts[trigram]
 
       bigram = first + "&" + second
       bigramcount = self.characterBigramCounts[bigram]
-      
+
       bigramScore = math.log(float(bigramcount + 1)) - math.log(len(sentence) + 26 ** 2)
       trigramScore = math.log(float(trigramcount + 1)) - math.log(len(sentence) + 26 ** 3)
-      # trigramScore = float(trigramcount + 1)/(bigramcount + len(self.characterBigramCounts) + 1)     
+      characterScore += trigramScore
 
-      # trigramScore = 0.0
-      # if trigramcount > 0:
-      #   #tri-gram
-      #   trigramScore = float(trigramcount)/bigramcount
-        # trigramScore += math.log(trigramcount) 
+
+    # wordScore = 0.0
+    # sentence = sentence.split()
+    # for i in xrange(0,len(sentence) - 2):
+    #   first = sentence[i]
+    #   second = sentence[i+1]
+    #   third = sentence[i+2]
+
+    #   trigram = first + "&" + second + "&" + third
+    #   trigramcount = self.trigramCounts[trigram]
+
+    #   bigram = first + "&" + second
+    #   bigramcount = self.bigramCounts[bigram]
+
+    #   trigramScore = 0.0
+    #   if trigramcount > 0:
+    #     #tri-gram
+    #     trigramScore = float(trigramcount)/bigramcount
+    #     # trigramScore += math.log(trigramcount)
         # trigramScore -= math.log(bigramcount)
 
       # Trigram works better without backing up
       # bigramScore = 0.0
-      # if bigramcount > 0:  
-      #   #Bi-gram
-      #   bigramScore += math.log(bigramcount)
-      #   bigramScore -= math.log(self.characterUnigramCounts[first])
-
-      # unigramScore = 0.0      
-      # unigramScore += math.log(self.characterUnigramCounts[second] + 1) 
-      # unigramScore -= math.log(self.characterUnigramTotal + len(self.characterUnigramCounts))  
-
-      characterScore += trigramScore # + unigramScore + bigramScore
-
-
-    wordScore = 0.0 
-    sentence = sentence.split()
-    for i in xrange(0,len(sentence) - 2):
-      first = sentence[i]
-      second = sentence[i+1]
-      third = sentence[i+2]
-      
-      trigram = first + "&" + second + "&" + third
-      trigramcount = self.trigramCounts[trigram]
-
-      bigram = first + "&" + second
-      bigramcount = self.bigramCounts[bigram]
-      
-      trigramScore = 0.0
-      if trigramcount > 0:
-        #tri-gram
-        trigramScore = float(trigramcount)/bigramcount
-        # trigramScore += math.log(trigramcount) 
-        # trigramScore -= math.log(bigramcount)
-
-      # Trigram works better without backing up
-      # bigramScore = 0.0
-      # if bigramcount > 0:  
+      # if bigramcount > 0:
       #   #Bi-gram
       #   bigramScore += math.log(bigramcount)
       #   bigramScore -= math.log(self.unigramCounts[first])
 
-      # unigramScore = 0.0      
-      # unigramScore += math.log(self.unigramCounts[second] + 1) 
-      # unigramScore -= math.log(self.unigramtotal + len(self.unigramCounts))  
+      # unigramScore = 0.0
+      # unigramScore += math.log(self.unigramCounts[second] + 1)
+      # unigramScore -= math.log(self.unigramtotal + len(self.unigramCounts))
 
-      wordScore += trigramScore # + unigramScore + bigramScore
+      # wordScore += trigramScore # + unigramScore + bigramScore;
 
     return characterScore
-    return wordScore + characterScore/100
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- solver.py
+++ solver.py
@@ -5,46 +5,11 @@ import math
 import string
 import LanguageModel
 import random
+import operator
 
 class HashableDict(dict):
     def __hash__(self):
         return hash(tuple(sorted(self.iteritems())))
-
-class SubstitutionProblem(util.SearchProblem):
-
-    def __init__(self, cipherText, cipherFreqMap, dictFreqMap):
-        self.cipherText = cipherText
-        self.cipherFreqMap = cipherFreqMap
-        self.dictFreqMap = dictFreqMap
-
-    def nextCharToMap(self, state):
-        mappedChars = state.keys()
-        for c in list(self.cipherText.upper()):
-            if c not in mappedChars and ord(c) < 91 and ord(c) >= 65: return c
-
-    def startState(self): return HashableDict()
-
-    def isGoal(self, state): return len(state) == 12
-
-    def succAndCost(self, state):
-        succ = []
-        cipherChar = self.nextCharToMap(state)
-        availableChars = set()
-        for i in xrange(65, 91): availableChars.add(chr(i))
-        availableChars -= set(state.values())
-        for c in availableChars:
-            newState = deepcopy(state)
-            newState[cipherChar] = c
-            # error margin
-            # weight = abs(self.cipherFreqMap[cipherChar] - self.dictFreqMap[c])/self.dictFreqMap[c]
-            # quotient
-            # weight = abs(self.cipherFreqMap[cipherChar] / self.dictFreqMap[c])
-            # difference
-            weight = abs(self.cipherFreqMap[cipherChar] - self.dictFreqMap[c])
-            succ.append(((cipherChar,c), newState, weight))
-
-        return succ
-
 
 class Solver:
     def __init__(self):
@@ -54,57 +19,35 @@ class Solver:
         self.charFreq = {"E":12.02,"T":9.10,"A":8.12,"O":7.68,"I":7.31,"N":6.95,"S":6.28,"R":6.02,"H":5.92,"D":4.32,"L":3.98,"U":2.88,"C":2.71,"M":2.61,"F":2.30,"Y":2.11,"W":2.09,"G":2.03,"P":1.82,"B":1.49,"V":1.11,"K":0.69,"X":0.17,"Q":0.11,"J":0.10,"Z":0.07}
 
     def decrypt(self, cipherText):
-        def swap(key):
-            key = list(key)
-            x = random.randrange(0,len(key))
-            y = random.randrange(0,len(key))
-            key[x], key[y] = key[y], key[x]
-            return "".join(key)
 
-        best_cipher_key = util.generateKey()
-        best_score = self.languagemodel.score(util.encrypt(cipherText, string.ascii_uppercase, best_cipher_key))
-        curr_cipher_key = best_cipher_key
-        curr_score = best_score
+        def swapIndices(a, b, key):
+            tempKey = list(key)
+            tempKey[a], tempKey[b] = tempKey[b], tempKey[a]
+            return "".join(tempKey)
+
+        def sample(swaps):            
+            r = random.random()
+            start = 0
+            for swap in swaps:
+                start += swap[0]
+                if r <= start: return swap
+
+        key = util.generateKey()
         for i in xrange(100000):
-            temp = swap(curr_cipher_key)
-            temp_score = self.languagemodel.score(util.encrypt(cipherText, string.ascii_uppercase, temp))
-            if temp_score > curr_score:
-              curr_cipher_key = temp
-              curr_score = temp_score
+            for a in xrange(len(key)):
+                swaps = []
+                for b in xrange(len(key)):
+                    temp_key = swapIndices(a, b, key)
+                    temp_score = self.languagemodel.score(util.encrypt(cipherText, string.ascii_uppercase, temp_key))
+                    swaps.append((temp_score, temp_key))
 
-              if temp_score > best_score:
-                best_cipher_key = temp
-                best_score = temp_score
-                print best_score, i, util.encrypt(cipherText, string.ascii_uppercase, temp)
+                # convert to probabilities
+                maxSwap = max(swaps, key=operator.itemgetter(0))
+                swaps = [(math.e ** (swap[0] - maxSwap[0]),swap[1]) for swap in swaps]
+                scoreSum = sum([swap[0] for swap in swaps])
+                swaps = [(float(swap[0])/scoreSum,swap[1]) for swap in swaps]
 
-            elif random.random() * best_score < temp_score:
-              curr_cipher_key = temp
-              curr_score = temp_score
+                # sample randomly
+                key = sample(swaps)[1]
 
-        return (util.encrypt(cipherText, string.ascii_uppercase, best_cipher_key), best_cipher_key)
-
-        # for k in self.charFreq: self.charFreq[k] /= 100.0
-
-        # cipherFreqMap = Counter()
-        # for c in list(cipherText.upper()): cipherFreqMap[c] += 1
-        # for c in cipherFreqMap.keys():
-        #     if ord(c) >= 91 or ord(c) < 65: del cipherFreqMap[c]
-        # total = sum(cipherFreqMap.values(), 0.0)
-        # for c in cipherFreqMap.keys(): cipherFreqMap[c] = cipherFreqMap[c] / total
-
-        # ucs = util.UniformCostSearch(verbose=0)
-        # ucs.solve(SubstitutionProblem(cipherText, cipherFreqMap, self.charFreq))
-
-        # print ucs.actions
-
-        # mapping = {}
-        # for a in ucs.actions: mapping[a[1]] = a[0]
-        # key = ""
-        # for i in xrange(65,91):
-        #     if chr(i) in mapping: key += mapping[chr(i)]
-        #     else: key += "?"
-
-        # print key
-        # decryptedText = util.encrypt(cipherText, string.ascii_uppercase, key)
-        # print decryptedText
-        # return (decryptedText, key)
+            print i, key, util.encrypt(cipherText, string.ascii_uppercase, key)
