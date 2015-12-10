554940d2e5f913257b04e46a87730336cdefedf0
Colin Man <colinman18@gmail.com>
2015-12-10 10:12:27 -0800

    add run sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+python decryptor.py -vt europarl-v7.es-en.en -n .05 >> europarl.results &
+python decryptor.py -vt newstest2012.en -n .05 >> newstest2012.results &
+python decryptor.py -vt newstest2013.en -n .05 >> newstest2013.results & 
+python decryptor.py -vt original -n .05 >> tee original.results &
+echo 'Running algorithm on testfiles in background. Call tail -f <file>.result to see logs as they come in'
